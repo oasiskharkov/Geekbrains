025f595a68d9fdd258d4404b593955fde91bcca7
Aleksei Klopkov <a.klopkov@skoltech.ru>
2021-05-18 11:30:23 +0300

    Lesson2: add Tic Tac Toe field initialization

--- les2/include/ticTacToe.h
+++ les2/include/ticTacToe.h
@@ -6,7 +6,7 @@ static constexpr size_t PNAME = 32;
 struct TicTacToe
 {
    enum CellType {EMPTY, CROSS, ZERO};
-   CellType field[FSIZE][FSIZE];
+   CellType field[FSIZE][FSIZE] = {};
    char playerA[PNAME];
    char playerB[PNAME];
    bool moveA;
